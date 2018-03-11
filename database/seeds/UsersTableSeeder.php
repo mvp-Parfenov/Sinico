48e83fba830cc80fb757fe8352be6e5675cec220
Maxim Parfenov <parfenov.maxim@pdffiller.team>
2018-03-11 23:21:17 +0200

    Added users table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Entity\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder {
+    public function run(): void
+    {
+        factory(User::class, 10)->create();
+    }
+}
