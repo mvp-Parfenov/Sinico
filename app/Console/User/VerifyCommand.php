d283e5d9f8d8f280b20d7fe151421e8c72822f89
Maxim Parfenov <parfenov.maxim@pdffiller.team>
2018-03-11 23:28:16 +0200

    Added user verify command

---
+++ app/Console/User/VerifyCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Console\User;
+
+
+use App\Entity\User;
+use Illuminate\Console\Command;
+use RegisterService;
+
+class VerifyCommand extends Command
+{
+    protected $signature = 'user:verify {email}';
+
+    protected $description = 'Verify user email';
+    /**
+     * @var RegisterService
+     */
+    private $service;
+
+
+    public function __construct(RegisterService $service)
+    {
+
+        parent::__construct();
+        $this->service = $service;
+    }
+
+    public function handle(): bool
+    {
+        $email = $this->argument('email');
+
+        if (!$user = User::where('email', $email)->first()) {
+            $this->error('Undefined user with email'.$email);
+
+            return false;
+        }
+
+        try {
+            $this->service->verify($user->id);
+        } catch (\DomainException $e) {
+            $this->error($e->getMessage());
+
+            return false;
+        }
+
+        $this->info('User is successfully verified');
+
+        return true;
+    }
+}
