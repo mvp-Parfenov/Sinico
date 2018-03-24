5f80d8f269d88e4d46b2385e4ba1f49f80cb0de1
Maxim Parfenov <parfenov.maxim@pdffiller.team>
2018-03-24 12:47:35 +0200

    Added category sorting

--- app/Http/Controllers/Admin/Adverts/CategoryController.php
+++ app/Http/Controllers/Admin/Adverts/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Adverts;
 use App\Entity\Adverts\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use function redirect;
 
 class CategoryController extends Controller
 {
@@ -110,6 +111,38 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         $category->delete();
+
+        return redirect()->route('admin.adverts.categories.index');
+    }
+
+    public function first(Category $category)
+    {
+        if($first = $category->siblings()->defaultOrder()->first()){
+            $category->insertBeforeNode($first);
+        }
+
+        return redirect()->route('admin.adverts.categories.index');
+    }
+
+    public function up(Category $category)
+    {
+        $category->up();
+
+        return redirect()->route('admin.adverts.categories.index');
+    }
+
+    public function down(Category $category)
+    {
+        $category->down();
+
+        return redirect()->route('admin.adverts.categories.index');
+    }
+
+    public function last(Category $category)
+    {
+        if($last = $category->siblings()->defaultOrder('desc')->first()){
+            $category->insertAfterNode($last);
+        }
 
         return redirect()->route('admin.adverts.categories.index');
     }
