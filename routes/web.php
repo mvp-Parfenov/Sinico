671f7f66e1252bfa0d38d798e932645d3788668f
Maxim Parfenov <parfenov.maxim@pdffiller.team>
2018-03-25 19:29:05 +0300

    Added cabinet route group

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
 Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
-Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
+
+Route::group(
+    [
+        'prefix' => 'cabinet',
+        'as' => 'cabinet.',
+        'namespace' => 'Cabinet',
+        'middleware' => ['auth'],
+    ],
+    function () {
+        Route::get('/', 'HomeController@index')->name('home');
+    }
+);
+
 Route::group(
     [
         'prefix' => 'admin',
